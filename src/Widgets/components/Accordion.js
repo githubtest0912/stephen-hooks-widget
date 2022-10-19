a04d08178f39ab12def8357759005caea29d0b54
Selvi <reachyourgoal2006@gmail.com>
2022-10-19 16:26:05 +0530

    add semantic link in index.html

--- src/Widgets/components/Accordion.js
+++ src/Widgets/components/Accordion.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Accordion = () => {
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    );
+  });
   return (
-    <div>
-            <h3>Accordion</h3>
+  <div className='ui styled accordion'>
+    {renderedItems}
     </div>
   )
-}
+};
 
-export default Accordion
+export default Accordion;
--- src/Widgets/components/MainFile.js
+++ src/Widgets/components/MainFile.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import Accordion from './Accordion'
 
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a frent end javascript library'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating component'
+    }
+]
+
 const MainFile = () => {
   return (
-    <div><Accordion /></div>
+    <div><Accordion items={items}/></div>
   )
 }
 
